22c2a75d4ed3a4460479c2a42f736787ce258316
Helene <helene.quintard@lapiscine.pro>
2023-12-19 15:53:00 +0100

    Add Update Coworking

--- src/page/admin/AdminCoworkingUpdate.jsx
+++ src/page/admin/AdminCoworkingUpdate.jsx
@@ -6,6 +6,8 @@ const AdminCoworkingUpdate = () =>{
     const { id } =useParams()
 // J'utilise la fonction useState je lui passe en paramètre la variable coworking dont la valeur par defaut est nulle.
     const [coworking, setCoworking] = useState (null)
+
+    const [message, setMessage] = useState(null)
 // J'utilise un useEffect pour qu'au chargement de la page le composant se recahrge qu'une fois
     useEffect(()=> {
 // Je déclare une fonction fléchée anonyme afin de récupérer l'url de l'id. Je convertis la réponse Json qui sera lu par le JS
@@ -18,11 +20,74 @@ const AdminCoworkingUpdate = () =>{
         })();
     },[])
 
+    const handleUpdateCoworking = async(event) => {
+        event.preventDefault();
+
+        
+
+        // Je déclare les variables des input de mon formulaire pour les faire matcher avec le modèle de mes coworkings du back
+            const name = event.target.name.value;
+            const priceByMonth = event.target.priceByMonth.value;
+            const priceByDay = event.target.priceByDay.value;
+            const priceByHour = event.target.priceByHour.value;
+            const addressNumber = event.target.addressNumber.value;
+            const addressStreet = event.target.addressStreet.value;
+            const addressCity = event.target.addressCity.value;
+            const addressPostcode = event.target.addressPostcode.value;
+            const superficy = event.target.superficy.value;
+            const capacity = event.target.capacity.value;
+    
+            // Je créé un objet
+            const coworkingToUpdate = {
+               
+                name: name,
+                price: {
+                hour: priceByHour,
+                day: priceByDay,
+                month: priceByMonth,
+                },
+                address: {
+                number: addressNumber,
+                street: addressStreet,
+                postCode: addressPostcode,
+                city: addressCity ,
+                },
+                superficy: superficy,
+                capacity: capacity,
+                };
+
+        // Je transforme mon objet en JSON
+        const coworkingToUpdateJson = JSON.stringify(coworkingToUpdate)
+        
+        // Je récupère mon token dans le local storage
+        const token = localStorage.getItem("jwt")
+
+        // J'envoie une requête avec la méthode PUT pour modifier un coworking dans laquelle je lui fais passer le headers (en format JSON), le token et le body en JSON
+        const coworkingToUpdateResponse = await fetch ('http://localhost:3000/api/coworkings/' +id,{
+            method: "PUT",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+            },
+            body: coworkingToUpdateJson,
+        });
+        // Si le statut de la réponse est égal à 201, un message de succès sera affiché
+        if ( coworkingToUpdateResponse.status === 201) {
+            setMessage ('Le Coworking a bien été modifié')
+        // Sinon un message d'erreur sera affiché
+        } else {
+            setMessage ('Erreur!')
+        }
+
+
+    }
+
 
     return (
         <div>
+          <>{message && <p>{message}</p>}</>
           {coworking && (
-            <form>
+            <form onSubmit={handleUpdateCoworking}>
               <div>
                 <label>
                   Nom
